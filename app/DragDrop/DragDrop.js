25a0b90e027911df0c5371876ed00ac11600dcfb
Taylor Bishop <ntaylorbishop@gmail.com>
2017-11-19 22:37:27 -0600

    nbishop: Refactor to move all code into the directive without polluting the global scope

--- app/DragDrop/DragDrop.js
+++ app/DragDrop/DragDrop.js
@@ -1,42 +1,52 @@
-var xMouseLoc = 0;
-var yMouseLoc = 0;
-var heldDownElementWidth = 0;
-var heldDownElementHeight = 0;
-var isElementHeldDown = false;
-var elementHeldDown;
-
-angular.module('DataStructureVisualizer').
-controller('DragDropController', function DragDropController($scope) {
-  $scope.captureCoordinate = function($event) {
-    xMouseLoc = $event.x;
-    yMouseLoc = $event.y;
-
-    if(isElementHeldDown == true) {
-      elementHeldDown.css('position', 'absolute');
-      elementHeldDown.css('left', xMouseLoc - heldDownElementWidth / 2 + 'px');
-      elementHeldDown.css('top', yMouseLoc - heldDownElementHeight / 2 + 'px');
-    }
-   }
-});
-
 angular.module('DataStructureVisualizer').
 directive('dragDrop', function () {
   return {
       link: function ($scope, element, attrs) {
-          element.bind('mousedown', function () {
-              element.css('background-color', 'rgb(17, 83, 252)');            
-              elementHeldDown = element;
-              isElementHeldDown = true;
+        var mouseLocX = 0;
+        var mouseLocY = 0;
+        var isElementHeldDown = false;
+        var elementHeldDown;
+        var heldDownElementWidth = 0;
+        var heldDownElementHeight = 0;
 
-              var style = window.getComputedStyle(element[0]);           
-              heldDownElementWidth = parseInt(style.getPropertyValue('width'));
-              heldDownElementHeight = parseInt(style.getPropertyValue('height'));              
-          });
-          element.bind('mouseup', function () {
-              element.css('background-color', 'rgb(73, 122, 255)');
-              isElementHeldDown = false;
-              console.log("mouse went up!");
-          });
+        //Bind callback to get mouse position
+        $scope.captureCoordinate = function($event) {
+            mouseLocX = $event.x;
+            mouseLocY = $event.y;
+
+            if(isElementHeldDown == true) {
+                elementHeldDown.css('position', 'absolute');
+                elementHeldDown.css('left', mouseLocX - heldDownElementWidth / 2 + 'px');
+                elementHeldDown.css('top', mouseLocY - heldDownElementHeight / 2 + 'px');
+            }
+        }
+
+        var dragDroppables = element[0].children;
+
+        for (var elementIdx = 0; elementIdx < dragDroppables.length; elementIdx++) {
+            var bindElement = dragDroppables[elementIdx];
+
+            var elementID = bindElement.attributes['id'].value;
+
+            if(elementID == "DragDroppable") {
+
+                var bindElementAngular = angular.element(bindElement);
+
+                bindElementAngular.on('mousedown', function() {
+                    bindElementAngular.css('background-color', 'rgb(17, 83, 252)');            
+                    elementHeldDown = bindElementAngular;
+                    isElementHeldDown = true;
+    
+                    var style = window.getComputedStyle(bindElementAngular);           
+                    heldDownElementWidth = parseInt(style.getPropertyValue('width'));
+                    heldDownElementHeight = parseInt(style.getPropertyValue('height'));     
+                });
+                bindElementAngular.on('mouseup', function () {
+                    bindElementAngular.css('background-color', 'rgb(73, 122, 255)');
+                    isElementHeldDown = false;
+                });
+            }
+        }
       }
   };
 });
---
+++ app/DragDrop/LineDraw.js
@@ -0,0 +1,36 @@
+
+angular.module('DataStructureVisualizer').
+controller('DragDropController', function DragDropController($scope) {
+  $scope.captureCoordinate = function($event) {
+    xMouseLoc = $event.x;
+    yMouseLoc = $event.y;
+
+    if(isElementHeldDown == true) {
+      elementHeldDown.css('position', 'absolute');
+      elementHeldDown.css('left', xMouseLoc - heldDownElementWidth / 2 + 'px');
+      elementHeldDown.css('top', yMouseLoc - heldDownElementHeight / 2 + 'px');
+    }
+   }
+});
+
+angular.module('DataStructureVisualizer').
+directive('dragDrop', function () {
+  return {
+      link: function ($scope, element, attrs) {
+          element.bind('mousedown', function () {
+              element.css('background-color', 'rgb(17, 83, 252)');            
+              elementHeldDown = element;
+              isElementHeldDown = true;
+
+              var style = window.getComputedStyle(element[0]);           
+              heldDownElementWidth = parseInt(style.getPropertyValue('width'));
+              heldDownElementHeight = parseInt(style.getPropertyValue('height'));              
+          });
+          element.bind('mouseup', function () {
+              element.css('background-color', 'rgb(73, 122, 255)');
+              isElementHeldDown = false;
+              console.log("mouse went up!");
+          });
+      }
+  };
+});
