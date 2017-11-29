03b7dbbec3f39f1d46a38c3bbe35af257a1f092e
Taylor Bishop <ntaylorbishop@gmail.com>
2017-11-28 21:14:54 -0600

    nbishop: setting up mongodb

--- server.js
+++ server.js
@@ -7,8 +7,17 @@ var bodyParser      = require('body-parser');    // pull information from HTML P
 var methodOverride  = require('method-override'); // simulate DELETE and PUT (express4)
 
 // configuration =================
+var dbURI = 'mongodb://localhost:27017/DataStructureVisualizer'; 
 
-//mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu');     // connect to mongoDB database on modulus.io
+mongoose.connection.on('connecting', function ()    { console.log('MongoDB: Trying: ' + dbURI);                             }); 
+mongoose.connection.on('connected', function ()     { console.log('MongoDB: Successfully connected to: ' + dbURI);          }); 
+mongoose.connection.on('error',function (err)       { console.log('MongoDB: ERROR connecting to: ' + dbURI + ' - ' + err);  }); 
+mongoose.connection.on('close',function (err)       { console.log('MongoDB: Connection Closed');                            }); 
+mongoose.connection.on('reconnected', function ()   { console.log('MongoDB: Database link was reconnected');                });
+mongoose.connection.on('disconnected', function ()  { console.log('MongoDB: The connection was ended on: ' + dbURI );       });
+mongoose.connection.on('open', function(ref)        { console.log("Connected to mongo server.");                            });
+mongoose.connect(dbURI);
+
 
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
@@ -17,13 +26,8 @@ app.use(bodyParser.json());                                     // parse applica
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
 
-// routes ==================================================================
-module.exports = function(app) {
-    // application -------------------------------------------------------------
-    app.get('*', function(req, res) {
-        res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
-};
+// routes ======================================================================
+require('./app/routes.js')(app);
 
 // listen (start app with node server.js) ======================================
 app.listen(8080);
